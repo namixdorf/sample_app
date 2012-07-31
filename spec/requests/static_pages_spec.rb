1914a77b0c5f6d8ca25113fcea24b8e12bb931f1
Nick Mixdorf <namixdorf@gmail.com>
2012-07-30 22:15:37 -0500

    add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,19 +2,7 @@ require 'spec_helper'
 
 describe "Static Pages" do
 
-	describe 'Home page' do
-
-		it "should have the content 'Sample App'" do
-			visit '/static_pages/home'
-			page.should have_content('Sample App')
-		end
-
-		it "should have the right title" do
-			visit '/static_pages/home'
-			page.should have_selector('title', 
-													text: "Ruby on Rails Tutorial Sample App | Home")
-		end
-	end
+	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
 	describe 'Help page' do
 
@@ -26,7 +14,7 @@ describe "Static Pages" do
 		it "should have the right title" do
 			visit '/static_pages/help'
 			page.should have_selector('title', 
-													text: "Ruby on Rails Tutorial Sample App | Help")
+													text: "#{base_title} | Help")
 		end
 	end
 
@@ -40,8 +28,40 @@ describe "Static Pages" do
 		it "should have the right title" do
 			visit '/static_pages/about'
 			page.should have_selector('title', 
-													text: "Ruby on Rails Tutorial Sample App | About Us")
+													text: "#{base_title} | About Us")
 		end
 	end
 
+	describe 'Contact page' do
+
+		it "should have the content 'Contact'" do
+			visit '/static_pages/contact'
+			page.should have_content('Contact')
+		end
+
+		it "should have the right tile" do
+			visit '/static_pages/contact'
+			page.should have_selector('title',
+														text: "#{base_title} | Contact")
+		end
+
+	end
+
+	describe "Home page" do
+
+		it "should have the h1 'Sample App'" do
+			visit '/static_pages/home'
+			page.should have_selector('h1', text: 'Sample App')
+		end
+
+		it "should have the base title" do
+			visit '/static_pages/home'
+			page.should have_selector('title', text: 'Ruby on Rails Tutorial Sample App')
+		end
+
+		it "should not have a custom page title" do
+			visit '/static_pages/home'
+			page.should_not have_selector('title', text: "| Home")
+		end
+	end
 end
